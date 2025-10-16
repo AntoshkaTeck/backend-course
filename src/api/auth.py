4d1b666d934e0969183825d13f29ba615fe8bc68
AntoshkTeck <anton.tihov.94@gmail.com>
2025-10-16 20:49:10 +1200

    user_login

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Body
+from datetime import datetime, timezone, timedelta
+
+from fastapi import APIRouter, Body, HTTPException, Response
 from fastapi.openapi.models import Example
 
 from pwdlib import PasswordHash
-
+import jwt
 
 from src.repositories.users import UsersRepository
 from src.database import async_session_maker
@@ -11,6 +13,22 @@ from src.schemas.users import UserRequestAdd, UserAdd
 router = APIRouter(prefix='/auth', tags=['Авторизация и Аутентификация'])
 
 password_hash = PasswordHash.recommended()
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode |= {"exp": expire}
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 @router.post('/register', summary="Добавление пользователя")
@@ -33,3 +51,30 @@ async def register_user(user_data: UserRequestAdd = Body(
         await session.commit()
 
     return {"satus": "OK"}
+
+
+@router.post('/login', summary="Аутентификация")
+async def login_user(
+        user_data: UserRequestAdd = Body(
+            openapi_examples={
+                "1": Example(
+                    summary="Anton",
+                    value={
+                        "email": "anton.tihov.94@gmail.com",
+                        "password": "qwerty12345678"
+                    }
+                )
+            }
+        ),
+        response: Response = Response()
+):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_user_with_hashed_password(email=user_data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail="Неправильный логин или пароль")
+        if not verify_password(user_data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail="Неправильный логин или пароль")
+        access_token = create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
+
+    return {"access_token": access_token}
