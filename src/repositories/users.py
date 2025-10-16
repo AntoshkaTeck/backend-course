7d73ce714c19be90053ea123d2181a88cee8f9b2
AntoshkTeck <anton.tihov.94@gmail.com>
2025-10-17 11:01:46 +1200

    refactor

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from pydantic import EmailStr
 from sqlalchemy import select
 
@@ -10,9 +11,11 @@ class UsersRepository(BaseRepository):
     model = UsersOrm
     schema = User
 
-    async def get_user_with_hashed_password(self, email: EmailStr, **filter_by):
+    async def get_user_with_hashed_password(self, email: EmailStr):
         query = select(self.model).filter_by(email=email)
         result = await self.session.execute(query)
-        model = result.scalars().one()
+        model = result.scalars().one_or_none()
+        if not model:
+            raise HTTPException(status_code=404, detail="Не правильный email или пароль")
 
         return UserWithHashedPassword.model_validate(model)
