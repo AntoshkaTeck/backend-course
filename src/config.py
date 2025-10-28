fc434df4e1aaa1cae96212103741b7e1ac2da97c
AntoshkTeck <anton.tihov.94@gmail.com>
2025-10-28 14:38:39 +1200

    redis install

--- src/config.py
+++ src/config.py
@@ -10,11 +10,14 @@ class Settings(BaseSettings):
 
     @property
     def DB_URL(self):
-       return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     JWT_SECRET_KEY: str
     JWT_ALGORITHM: str
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    REDIS_HOST: str
+    REDIS_PORT: int
 
     model_config = SettingsConfigDict(env_file=".env")
 
