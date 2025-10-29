b3f4729c87e151e5902058bbd28bc2ec8254ef91
AntoshkTeck <anton.tihov.94@gmail.com>
2025-10-29 12:03:22 +1200

    image resize celery rask

--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -1,3 +1,6 @@
+import os.path
+
+from PIL import Image, ImageOps
 from time import sleep
 
 from src.tasks.celery_app import celery_instance
@@ -7,3 +10,25 @@ from src.tasks.celery_app import celery_instance
 def test_task():
     sleep(5)
     print("Test task")
+
+
+@celery_instance.task
+def resize_image(image_path: str):
+    sizes = [1000, 500, 200]
+    output_folder = "src/static/images"
+
+    img = Image.open(image_path)
+    # 💡 Исправляем ориентацию по EXIF
+    img = ImageOps.exif_transpose(img)
+
+    base_name = os.path.basename(image_path)
+    name, ext = os.path.splitext(base_name)
+
+    for size in sizes:
+        img_resized = img.resize((size, int(img.height * (size / img.width))), Image.Resampling.LANCZOS)
+
+        new_file_name = f"{name}_{size}px_{ext}"
+        output_path = os.path.join(output_folder, new_file_name)
+        img_resized.save(output_path)
+
+    print(f"Resized image to {output_folder}")
