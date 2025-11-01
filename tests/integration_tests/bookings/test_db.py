40026f3da104427e3b4bb1531154be0b52a83a47
AntoshkTeck <anton.tihov.94@gmail.com>
2025-11-01 13:21:27 +1200

    refactor

--- tests/integration_tests/bookings/test_db.py
+++ tests/integration_tests/bookings/test_db.py
@@ -13,16 +13,26 @@ async def test_booking_crud(db):
         date_to=date(year=2025, month=12, day=20),
         price=2500
     )
-    booking = await db.bookings.add(booking_data)
+    new_booking = await db.bookings.add(booking_data)
+
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
     assert booking
+    assert booking.id == new_booking.id
 
     new_booking_data = BookingAdd(
         user_id=user_id,
         room_id=room_id,
-        date_from=date(year=2025, month=12, day=12),
+        date_from=date(year=2025, month=12, day=1),
         date_to=date(year=2025, month=12, day=31),
         price=2500
     )
-    await db.bookings.update(new_booking_data, id= booking.id)
+    await db.bookings.update(new_booking_data, id= new_booking.id)
+    updated_booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert updated_booking.date_from == date(year=2025, month=12, day=1)
+
     await db.bookings.delete(id=booking.id)
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+
+    assert not booking
+
     await db.commit()
