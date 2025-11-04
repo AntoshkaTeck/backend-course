2ddba7e5b02b811a2801e731d2561aa28abe7232
AntoshkTeck <anton.tihov.94@gmail.com>
2025-11-05 10:55:23 +1200

    add_booking with ids available

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -25,7 +25,7 @@ async def create_booking(
 ):
     room = await db.rooms.get_one_or_none(id=booking_data.room_id)
     _booking_data = BookingAdd(user_id=user_id, **booking_data.model_dump(), price=room.price)
-    booking = await db.bookings.add(_booking_data)
+    booking = await db.bookings.add_booking(_booking_data)
     await db.commit()
 
     return {"status": "OK", "data": booking}
--- src/repositories/bookings.py
+++ src/repositories/bookings.py
@@ -1,10 +1,13 @@
 from datetime import date
 
+from fastapi import HTTPException
 from sqlalchemy import select
 
 from src.models.bookings import BookingsOrm
 from src.repositories.base import BaseRepository
 from src.repositories.mappers.mappers import BookingDataMapper
+from src.repositories.utils import rooms_ids_for_booking
+from src.schemas.bookings import BookingAdd
 
 
 class BookingsRepository(BaseRepository):
@@ -19,3 +22,15 @@ class BookingsRepository(BaseRepository):
         res = await self.session.execute(query)
 
         return [self.mapper.map_to_domain_entity(booking) for booking in res.scalars().all()]
+
+    async def add_booking(self, booking_data: BookingAdd):
+        query = rooms_ids_for_booking(
+            date_from=booking_data.date_from,
+            date_to=booking_data.date_to,
+        )
+        result = await self.session.execute(query)
+        availability_ids = result.scalars().all()
+        if booking_data.room_id not in availability_ids:
+            raise HTTPException(status_code=403, detail="Бронирование запрещено")
+
+        return await self.add(booking_data)
