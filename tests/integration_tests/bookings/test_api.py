2cb4062a95f2484f64060213930669e67d9c1e0c
AntoshkTeck <anton.tihov.94@gmail.com>
2025-11-05 11:49:33 +1200

    параметризация тестов

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,16 +1,31 @@
-async def test_add_booking(auth_ac, db):
-    room_id = (await db.rooms.get_all())[0].id
+import pytest
+
+
+@pytest.mark.parametrize("room_id, date_from, date_to, status_code", [
+    (1, "2025-11-01", "2025-11-30", 200),
+    (1, "2025-11-01", "2025-11-30", 200),
+    (1, "2025-11-01", "2025-11-30", 200),
+    (1, "2025-11-01", "2025-11-30", 200),
+    (1, "2025-11-01", "2025-11-30", 200),
+    (1, "2025-11-01", "2025-11-30", 403),
+])
+async def test_add_booking(room_id,
+                           date_from,
+                           date_to,
+                           status_code,
+                           auth_ac, db):
     response = await auth_ac.post(
         "/bookings/",
         json={
             "room_id": room_id,
-            "date_from": "2025-11-01",
-            "date_to": "2025-11-30",
+            "date_from": date_from,
+            "date_to": date_to,
 
         }
     )
-    assert response.status_code == 200
-    res = response.json()
-    assert res["status"] == "OK"
-    assert isinstance(res, dict)
-    assert "data" in res
+    assert response.status_code == status_code
+    if status_code == 200:
+        res = response.json()
+        assert res["status"] == "OK"
+        assert isinstance(res, dict)
+        assert "data" in res
